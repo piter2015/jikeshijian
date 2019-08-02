21b7da9259b2764a94ab5b3ca7621450a3249da8
li renpeng <85835578@qq.com>
2019-08-02 21:39:45 +0800

    增加test页面

---
+++ jikeshijian0803/src/components/test.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="app-container">
+
+ <div class="filter-container">
+ 搜索栏
+ </div>
+<div class="filter-container">
+  <br>
+<el-tag type="danger">内容板块</el-tag>
+
+</div> 
+
+  </div>
+</template>
+
+<script>
+export default {
+components: {  },
+  name: "test",
+
+  data() {
+    return {    
+      userID: 0,
+      dialogStatus: "create",    
+      listQuery: {
+        page: 1,
+        limit: 10,
+        importance: undefined,
+        title: null,
+        type: undefined,
+     
+      },
+    };
+  },
+  created() {
+   
+  },
+   watch: {
+    userID: function() {
+       if (this.userID == 0)
+   { this.resetTemp()}
+   else
+   {
+     
+    }}
+  },
+
+   props: {
+    userID: {
+      type: Number,
+      default: 0
+    },
+    dialogStatus: {
+      type: String,
+      default: "create"
+    }
+  },
+  methods: {
+
+    getList() {
+
+   
+     
+    },
+
+  
+  }
+};
+</script>
+
+<style>
+
+</style>
